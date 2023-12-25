0a4703c231c1e8430e047327e5609766df65cb6b
Vladimir Palamar <vladimir.palamar@lenta.com>
2023-12-25 19:43:22 +0300

    chore: insert code

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,18 +1,17 @@
 package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
-import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    private final SelenideElement loginField = $("[data-test-id=login] input");
-    private final SelenideElement passwordField = $("[data-test-id=password] input");
-    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
 
-    public VerificationPage validLogin(DataHelper.AuthInfo info) {
-        loginField.setValue(info.getLogin());
-        passwordField.setValue(info.getPassword());
+    public VerificationPage validLogin(String login, String password) {
+        loginField.setValue(login);
+        passwordField.setValue(password);
         loginButton.click();
         return new VerificationPage();
     }
