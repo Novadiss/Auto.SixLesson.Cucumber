659fbbc42b4beed25ab346cbfccee91434a8332c
Vladimir Palamar <vladimir.palamar@lenta.com>
2024-01-07 20:02:50 +0300

    chore: rewrite code

--- src/test/java/ru/netology/steps/TemplatesStepV2.java
+++ src/test/java/ru/netology/steps/TemplatesStepV2.java
@@ -6,8 +6,9 @@ import io.cucumber.java.ru.Тогда;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
-import ru.netology.page.TransferPage;
 import ru.netology.page.VerificationPage;
+
+import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,21 +32,26 @@ public class TemplatesStepV2 {
         dashboardPage = verificationPage.validVerify("12345");
     }
 
-    @Когда("когда пользователь переводит 5000 рублей с карты с номером 5559 0000 0000 0002 на свою 1 карту с главной страницы")
-    public void transferFromFirstToSecond() {
-        var amount = 5000;
+    @Когда("когда пользователь переводит {string} рублей с карты с номером {string} на свою 1 карту с главной страницы")
+    public void transferFromFirstToSecond(String amount, String cardNum) {
+        //var amount = 5000;
         firstCardInfo = getFirstCardInfo();
         secondCardInfo = getSecondCardInfo();
         firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
         secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
-        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
-        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+        if (Objects.equals(cardNum, "5559 0000 0000 0002")) {
+            var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+            dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+        } else {
+            var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+            dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        }
     }
 
-    @Тогда("тогда баланс его 1 карты из списка на главной странице должен стать 15000 рублей")
-    public void actualBalanceFirstCard() {
-        var expectedBalanceFirstCard = 15_000;
-        var actualBalanceFirstCard = dashboardPage.getCardBalance(DataHelper.getFirstCardInfo());
+    @Тогда("тогда баланс его 1 карты из списка на главной странице должен стать {string} рублей")
+    public void actualBalanceFirstCard(String balance) {
+        int expectedBalanceFirstCard = Integer.parseInt(balance);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(getFirstCardInfo());
         assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
     }
 }
